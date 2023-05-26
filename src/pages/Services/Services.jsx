321c34003722c031b2b547dd96ad94f026ed2531
mohi14 <mohiuddinkhan1409@gmail.com>
2023-05-26 19:01:37 +0600

    our services done

---
+++ src/pages/Services/Services.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <section className="container mx-auto mt-[146px]">
+      <h4 className="font-[500] text-[#00B3AD] text-[20px]">Our Services</h4>
+      <h3 className="mb-[70px] font-[600] text-[#20226A] text-[40px]">
+        Our Consulting Specialist
+      </h3>
+    </section>
+  );
+};
+
+export default Services;
