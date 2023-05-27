19052ed8a5e67b38b0c5ea7d69477f96300eb299
mohi14 <mohiuddinkhan1409@gmail.com>
2023-05-27 10:44:40 +0600

    our services section done

--- src/components/Services/ServicesCard.jsx
+++ src/components/Services/ServicesCard.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./ServicesCard.css";
 
-const ServicesCard = () => {
-  return <div></div>;
+const ServicesCard = ({ item }) => {
+  const { image, title, content } = item;
+  return (
+    <div className="p-[35px] border-[2px] border-[#D2EFFF] rounded-[25px] hover:bg-[#1379B1] cursor-pointer services ">
+      <img src={image} alt="" className="bg-[#E3EFF6] p-[19px] rounded-full " />
+      <h3 className="font-[500] text-[24px] text-[#20226A] mt-[30px] mb-[15px]">
+        {title}
+      </h3>
+      <p className="font-[400] text-[16px] text-[#656765]">{content}</p>
+    </div>
+  );
 };
 
 export default ServicesCard;
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,12 +1,82 @@
 import React from "react";
+import rightArrow from "../../assets/icons/right-arrow.png";
+import ServicesCard from "../../components/Services/ServicesCard";
+import siring from "../../assets/icons/siringe.png";
+import lung from "../../assets/icons/lung.png";
+import capsule from "../../assets/icons/capsule.png";
+import head from "../../assets/icons/head.png";
+import pen from "../../assets/icons/pen.png";
+import bottle from "../../assets/icons/bottole.png";
+import hand from "../../assets/icons/hand.png";
+import heart from "../../assets/icons/heart.png";
+
+const specialist = [
+  {
+    image: siring,
+    title: "Covid-19 Test",
+    content:
+      "Eleifend enim, natoque bulum, nibh dui mi adipiscing dui ndum ultrices tincidunt",
+  },
+  {
+    image: lung,
+    title: "Heart Lungs",
+    content:
+      "Eleifend enim, natoque bulum, nibh dui mi adipiscing dui ndum ultrices tincidunt",
+  },
+  {
+    image: capsule,
+    title: "Vitamin & Suplement",
+    content:
+      "Eleifend enim, natoque bulum, nibh dui mi adipiscing dui ndum ultrices tincidunt",
+  },
+  {
+    image: head,
+    title: "Mental Health",
+    content:
+      "Eleifend enim, natoque bulum, nibh dui mi adipiscing dui ndum ultrices tincidunt",
+  },
+  {
+    image: pen,
+    title: "General Surgery",
+    content:
+      "Eleifend enim, natoque bulum, nibh dui mi adipiscing dui ndum ultrices tincidunt",
+  },
+  {
+    image: bottle,
+    title: "Pharmacy",
+    content:
+      "Eleifend enim, natoque bulum, nibh dui mi adipiscing dui ndum ultrices tincidunt",
+  },
+  {
+    image: hand,
+    title: "Sports Injury",
+    content:
+      "Eleifend enim, natoque bulum, nibh dui mi adipiscing dui ndum ultrices tincidunt",
+  },
+  {
+    image: heart,
+    title: "Heart Diseases",
+    content:
+      "Eleifend enim, natoque bulum, nibh dui mi adipiscing dui ndum ultrices tincidunt",
+  },
+];
 
 const Services = () => {
   return (
-    <section className="container mx-auto mt-[146px]">
+    <section className="container mx-auto mt-[146px] mb-[150px]">
       <h4 className="font-[500] text-[#00B3AD] text-[20px]">Our Services</h4>
       <h3 className="mb-[70px] font-[600] text-[#20226A] text-[40px]">
         Our Consulting Specialist
       </h3>
+      <p className="font-[400] text-[16px] text-[#00B3AD] text-end mb-[30px] cursor-pointer">
+        View all{" "}
+        <img src={rightArrow} alt="" className="inline h-[20px] w-[20px]" />
+      </p>
+      <div className="grid grid-cols-4 gap-[30px]">
+        {specialist.map((item, idx) => (
+          <ServicesCard item={item} />
+        ))}
+      </div>
     </section>
   );
 };
