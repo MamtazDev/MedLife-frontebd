9951d7a973f5e768f5cc2e9f785cb057dee88b15
mohi14 <mohiuddinkhan1409@gmail.com>
2023-05-29 15:04:20 +0600

    testimonial section done

---
+++ src/components/Testimonials/TestimonialsCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import sliderDoctor from "../../assets/images/sliderDoctor.png";
+import icon from "../../assets/icons/squareIcon.png";
+import frame from "../../assets/images/frame.png";
+import "./TestimonialsCard.css";
+
+const TestimonialsCard = () => {
+  return (
+    <div className="flex testimonial-box justify-between">
+      <div className="w-1/2">
+        <h3 className="font-[600] text-[36px] text-[#20226A] mb-[30px]">
+          Inventore vel alias tel consequatur nihil vel maiores voluptate enim
+          ea deleniti.
+        </h3>
+        <div className="flex items-center gap-2">
+          <img src={icon} alt="" />
+          <div>
+            <h6 className="font-[700] text-[18px] text-[#00B3AD]">
+              Sherri Cronin
+            </h6>
+            <p className="font-[600] text-[14px] text-[#656765]">
+              Dynamic Doctor
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="w-1/2">
+        <img src={sliderDoctor} alt="" className="ml-auto" />
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialsCard;
--- src/pages/Testimonials/Testimonials.jsx
+++ src/pages/Testimonials/Testimonials.jsx
@@ -1,14 +1,86 @@
 import React from "react";
 
+// import Swiper core and required modules
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Keyboard,
+  Mousewheel,
+} from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "./Testimonials.css";
+import TestimonialsCard from "../../components/Testimonials/TestimonialsCard";
 const Testimonials = () => {
   return (
-    <section className="container mx-auto">
+    <section className="container mx-auto mb-[125px]">
       <p className="text-center font-[500] text-[20px] text-[#00B3AD] mb-[5px]">
         Testimonials
       </p>
       <h3 className="text-center font-[600] text-[40px] text-[#20226A] mb-[70px]">
         What Clients Say
       </h3>
+
+      <div className="frame-box max-w-[1048px] mx-auto p-[50px]">
+        <div className="testi__swiper  w-[998px] mx-auto  bg-white ">
+          <Swiper
+            // install Swiper modules
+            modules={[Mousewheel, Pagination, Navigation]}
+            direction={"horizontal"}
+            spaceBetween={30}
+            slidesPerView={1}
+            navigation={{ clickable: true }}
+            Mousewheel={true}
+            pagination={{ clickable: true }}
+            // scrollbar={{ draggable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log("slide change")}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              {" "}
+              <TestimonialsCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              {" "}
+              <TestimonialsCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              {" "}
+              <TestimonialsCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              {" "}
+              <TestimonialsCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              {" "}
+              <TestimonialsCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              {" "}
+              <TestimonialsCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              {" "}
+              <TestimonialsCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              {" "}
+              <TestimonialsCard />
+            </SwiperSlide>
+          </Swiper>
+          {/* <TestimonialsCard />s */}
+        </div>
+      </div>
     </section>
   );
 };
